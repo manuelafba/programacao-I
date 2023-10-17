4e6aebbd944c4cb26525a4744dee9212cb3ccb44
Manuela Araújo <mxnuelafba@gmail.com>
2023-10-16 22:04:50 -0300

    questão 16

---
+++ 1ª avaliação/lista 1 funções/ex16.py
@@ -0,0 +1,8 @@
+from random import randint
+
+def verificar_frequencia_lista(lista, valor_referencia):
+    return lista, lista.count(valor_referencia)
+
+lista_aleatoria = [randint(1, 100) for _ in range(1000)]
+valor_referencia = randint(1, 100)
+print(verificar_frequencia_lista(lista_aleatoria, valor_referencia))
