20abce8171bcdf8fd493022f452032a6c85315fe
Manuela Araújo <mxnuelafba@gmail.com>
2023-10-16 21:51:46 -0300

    list comprehension

--- 1ª avaliação/lista 1 contagem/ex18.py
+++ 1ª avaliação/lista 1 contagem/ex18.py
@@ -11,4 +11,11 @@ sequencia1 = [0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0]
 sequencia2 = [0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0]
 print(verificar_distancia_hamming(sequencia1, sequencia2))
 
+def verificar_distancia_hamming2(sequencia1, sequencia2):
+    return sum(1 for x, y in zip(sequencia1, sequencia2) if x != y)
+
+sequencia1 = [1, 0, 0, 1, 1, 0, 1, 0]
+sequencia2 = [1, 1, 1, 1, 1, 1, 1, 1]
+print(verificar_distancia_hamming2(sequencia1, sequencia2))
+
 
