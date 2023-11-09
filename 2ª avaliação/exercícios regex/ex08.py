947c88a844c1e9c578e36b0bb9585578e6ec1b19
manuela araújo <104527846+manuelafba@users.noreply.github.com>
2023-11-09 19:10:25 +0000

    update

--- 2ª avaliação/exercícios regex/ex08.py
+++ 2ª avaliação/exercícios regex/ex08.py
@@ -2,8 +2,8 @@ import re
 
 string = input("Escreva uma operação aritmética entre números inteiros: ")
 
-saida = re.search(r'[0-9]', string) and\
-    re.search(r'[+_*/=]', string) and\
+saida = re.search(r'^[0-9]+', string) and\
+    re.search(r'^[+_*/=]+', string) and\
     not re.search(r"[.,]", string)
 
 if saida:
